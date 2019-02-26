154712f7adeab92f93902b632521d626a0856a24
Multimegamander <multimegamander@gmail.com>
2019-02-26 09:42:29 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+$('document').ready(function(){
+  $('body').on("show.bs.collapse", ".qr-collapse", function(event){
+    var title = angular.element(event.target).scope().title;
+    $(event.target).qrcode({text:'input});
+  });
 
+  $('body').on("hidden.bs.collapse", ".qr-collapse", function(event){
+    $(event.target).empty();
+  });
+});
